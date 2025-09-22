a4ac671e96057d6cf5a2e8d6bfdacdf8e9fd51e1
nicokijoue <nicokijoue@gmail.com>
2025-09-22 02:59:04 +0200

    fonction aleatoire pour emplacement des mines

--- minesweeper.py
+++ minesweeper.py
@@ -5,6 +5,16 @@ def affichage ():
     for row in tableau_affichage :
         print(row)
 
+
+def aléatoire (tableau, nbr_mines):
+    positions_prises = []
+    while len(positions_prises) < nbr_mines:
+        i = random.randint(0, 9)  
+        j = random.randint(0, 9) 
+        if (i, j) not in positions_prises:
+            tableau[i][j] = 1
+            positions_prises.append((i, j))
+
 tableau_data = [
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
