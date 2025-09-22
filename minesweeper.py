8ff65d9a888f4a10cec81bbcbfd404e66b88f183
nicokijoue <nicokijoue@gmail.com>
2025-09-22 04:04:52 +0200

    entrer des coordonnees + correction d'orthographe

--- minesweeper.py
+++ minesweeper.py
@@ -2,8 +2,8 @@ import random
 
 
 def affichage ():
-    for row in tableau_affichage :
-        print(row)
+    for ligne in tableau_affichage :
+        print(ligne)
 
 
 def aléatoire (tableau, nbr_mines):
@@ -14,6 +14,20 @@ def aléatoire (tableau, nbr_mines):
         if (i, j) not in positions_prises:
             tableau[i][j] = 1
             positions_prises.append((i, j))
+
+
+def coordonnée():
+    while True:
+        try:
+            ligne = int(input("entrer une ligne entre 0-9 (0 et 9 comprit): "))
+            colonne = int(input("enter une colonne entre 0-9 (0 et 9 comprit): "))
+            if 0 <= ligne <= 9 and 0 <= colonne <= 9:
+                return ligne, colonne
+            else:
+                print("veuillez mettre un nombre entre 0-9 (0 et 9 comprit)")
+        except ValueError:
+            print("veuillez enter un nombre et non autre chose.")
+
 
 tableau_data = [
     [0,0,0,0,0,0,0,0,0,0],
